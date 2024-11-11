b2d92e0a8dc71e5548ffeeebe338d00044200f25
daviemaria <eriickdavi7@gmail.com>
2024-11-11 14:42:52 -0300

    Add files via upload

---
+++ conversor/A.java
@@ -0,0 +1,12 @@
+🎉 Usuario Criado 🎉
+🔎 Usuario: usuariojeh
+
+🔑 Senha: 123456789
+
+🎯 Validade: 07/12/2024
+
+🕟 Limite: 1
+
+⛔PROIBIDO ⛔ Uso de Torrent/Utorrent - ⛔PROIBIDO USAR O APLICATIVO PARA FINS ILÍCITOS ⛔ Obrigado por usar nosso serviço!!
+
+
---
+++ conversor/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+
+     public static void main (String[] args) {
+         Scanner scanner = new Scanner(System.in);
+
+         System.out.println("Bem-vindo ao Conversor de Moedas!");
+         System.out.println("Escolha a conversão desejada:");
+         System.out.println("1. USD para EUR");
+         System.out.println("2.EUR para USD");
+         System.out.println("USD para BRL");
+         System.out.println("BRL para USD");
+         System.out.println("EUR para BRL");
+         System.out.println("BRL para EUR");
+         System.out.println("Digite o número da conversão: ");
+
+         int opcao = scanner.nextInt();
+
+         System.out.println("Digite o valor para conversão");
+         double valor = scanner.nextDouble();
+
+
+
+
+
+
+
+
+
+
+
+     }
+
+
+
+}
